ddc69bc08441db8f75083e3c173be50611a482d7
Trey <tajohns22@gmail.com>
2020-06-06 14:02:56 -0500

    setup app file

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+#use the local db if environ DB_URI variable is not set
+app.config["SQLALCHEMY_DATABASE_URI"] = ( os.environ.get("Database_URL", "postgres:///mydroplist"))
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "this is my secret")
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route("/")
+def homepage():
+    """Home page of the application"""
+    return("Hello World")
+
+# @app.after_request
+# def add_header(req):
+#     "make each request header non-caching."
+
+#     req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     req.headers["Pragma"] = "no-cache"
+#     req.headers["Expires"] = "0"
+#     req.headers["Cache-Control"] = "public, max-age=0"
+#     return req
