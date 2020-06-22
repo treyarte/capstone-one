e45d7250669ffe57959ec11947339852068d39c9
Trey <tajohns22@gmail.com>
2020-06-22 15:48:43 -0500

    add test droplist items index

--- test_droplist_items_routes.py
+++ test_droplist_items_routes.py
@@ -23,7 +23,7 @@ class DroplistItemsViewsTestCase(TestCase):
 
         self.client = app.test_client()
 
-        u1, u2, u3, s1, s2, f1, droplist = droplist_setup()
+        u1, u2, u3, s1, s2, f1, droplist, droplist_2, droplist_3 = droplist_setup()
 
         self.u1 = u1
         self.u2 = u2
@@ -32,15 +32,47 @@ class DroplistItemsViewsTestCase(TestCase):
         self.s2 = s2
         self.f1 = f1
         self.droplist = droplist
+        self.droplist_2 = droplist_2
 
        
         loc = Location(name="S409")
+        loc_2 = Location(name="S408")
+        loc_3 = Location(name="113")
 
-        db.session.add(loc)
+        db.session.add_all([loc, loc_2, loc_3])
         db.session.commit()
 
         self.loc = loc
+        self.loc_2 = loc_2
+        self.loc_3 = loc_3
         
+        item_1 = Item(
+                            row_letter="a", 
+                            column_number=1, 
+                            location_id=self.loc.id, 
+                            description="Test Item 1", 
+                            droplist_id=self.droplist_2.id)
+
+        item_2 = Item(
+                            row_letter="a", 
+                            column_number=2, 
+                            location_id=self.loc_2.id, 
+                            description="Test Item 2", 
+                            droplist_id=self.droplist_2.id)
+
+        item_3 = Item(
+                            row_letter="c", 
+                            column_number=3, 
+                            location_id=self.loc_3.id, 
+                            description="Test Item 3", 
+                            droplist_id=self.droplist_2.id)
+        
+        db.session.add_all([item_1, item_2, item_3])
+        db.session.commit()
+
+        self.item_1 = item_1
+        self.item_2 = item_2
+        self.item_3 = item_3
 
     def tearDown(self):
         """Rollback transactions"""
@@ -67,12 +99,28 @@ class DroplistItemsViewsTestCase(TestCase):
             item = Item.query.filter(Item.description=="test strawberries").first()
             self.assertIn(item,droplist.droplist_items)
 
-    def test_droplist_index(self)
-    
-    def test_droplist_show_item(self)
+    def test_droplist_index(self):
+        """Test if a user can view items in a droplist"""
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1.id
+                self.droplist_id = self.droplist_2.id
 
-    def test_droplist_edit_item(self)
+            resp = c.get(f"/droplists/{self.droplist_id}/items")
+            html = resp.get_data(as_text=True)
+            
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Test Item 1", html)
+            self.assertIn("Test Item 2", html)
+            self.assertIn("Test Item 3", html)
 
-    def test_droplist_delete_item(self)
 
-    
+
+
+
+    # def test_droplist_show_item(self)
+
+    # def test_droplist_edit_item(self)
+
+    # def test_droplist_delete_item(self)
+
