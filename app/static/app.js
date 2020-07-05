a7ee1e214ffb5b6e16cceb20a42e95b529db6411
Trey <tajohns22@gmail.com>
2020-07-05 14:33:26 -0500

    complete 404 page

--- app/static/app.js
+++ app/static/app.js
@@ -1,20 +1,6 @@
-// const BASE_URL = 'http://localhost:5000/api';
-const BASE_URL = 'https://mydroplist.herokuapp.com//api';
-
-$graph = $('.all-forklifts');
+const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'https://mydroplist.herokuapp.com//api';
 
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-async function get_graph() {
-  let status = $('#chart-status').val();
-  let type = $('#chart-type').val();
-  const resp = await axios.post(`${BASE_URL}/forklift_drivers/droplists`, {
-    status,
-    type,
-  });
-  $('.graph-content').empty().append(`<img src="${resp.data}" class="chart"/>`);
-}
-
-$graph.on('click', get_graph);
---
+++ app/static/statistics.js
@@ -0,0 +1,13 @@
+$graph = $('.all-forklifts');
+
+async function get_graph() {
+  let status = $('#chart-status').val();
+  let type = $('#chart-type').val();
+  const resp = await axios.post(`${BASE_URL}/forklift_drivers/droplists`, {
+    status,
+    type,
+  });
+  $('.graph-content').empty().append(`<img src="${resp.data}" class="chart"/>`);
+}
+
+$graph.on('click', get_graph);
